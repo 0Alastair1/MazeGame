5e1eda13020fc72def42fa85244490d7c124f6be
0Alastair1 <96997148+0Alastair1@users.noreply.github.com>
2022-11-16 20:52:24 +0000

    Revert "small bug fix"
    This reverts commit 75aec51a71dc140b3ce8d48420b13c0fa2bbfeb8.

--- src/gameEngine/renderEngine/textures.cpp
+++ src/gameEngine/renderEngine/textures.cpp
@@ -1,6 +1,5 @@
 static inline void genTextures() //textures are made automaticlly when added into the textures folder
 {
-    /*
     boost::filesystem::path currentDirPath = boost::dll::program_location().parent_path();
     const char* currentDir = currentDirPath.string().c_str();
     const char* texturesFolder = "/textures/";
@@ -19,25 +18,22 @@ static inline void genTextures() //textures are made automaticlly when added int
 
         textureNames.push_back(std::filesystem::path{entry.path()}.filename().string().c_str());
     }
-    */
-   //textureNames.push_back("wood.png");
 
     stbi_set_flip_vertically_on_load(1);
 
-    //for(const char* textureName : textureNames)
-    //{
+    for(const char* textureName : textureNames)
+    {
         
-        //char* filePath = (char*)malloc(strlen(textureDirectory) + strlen(textureName));
-        //strcpy(filePath,(char*)textureDirectory);
-        //strcat(filePath,(char*)textureName);
-        //free(textureDirectory);
-        const char* filePath = "/home/computer/Documents/GitHub/MazeGame/build/textures/wood.png";
+        char* filePath = (char*)malloc(strlen(textureDirectory) + strlen(textureName));
+        strcpy(filePath,(char*)textureDirectory);
+        strcat(filePath,(char*)textureName);
+        free(textureDirectory);
 
         int width = 0;
         int height = 0;
         int bpp = 0;
         unsigned char* buffer = stbi_load(filePath, &width, &height, &bpp, 4);
-        //free(filePath);
+        free(filePath);
 
         unsigned int textureID = 0;
 
@@ -58,9 +54,9 @@ static inline void genTextures() //textures are made automaticlly when added int
 
         textureObject* texture = new textureObject;
         texture->textureID = textureID;
-        texture->textureName = "wood.png";
+        texture->textureName = textureName;
         textureObjects.push_back(texture);
-   // }
+    }
 }
 
 static inline textureObject* getTexture(const char* textureName)
